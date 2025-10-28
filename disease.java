2757fffd9fa99f3a471c51f50dffdc6da3cd7760
Afsheen0524 <afsheen.manha24@gmail.com>
2025-10-28 10:55:53 +0530

    Create disease.java

---
+++ disease.java
@@ -0,0 +1,10 @@
+public class ask{
+  public static void main(String[] args){
+    System.out.println("COVID TRACKER");
+    System.out.println("Enter input:deaths");
+    System.out.println("7010681");
+    System.out.println("Enter input:population");
+    System.out.println("7944935131");
+  }
+}
+
